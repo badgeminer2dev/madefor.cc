de16e5363c1d8b6f28fe4075969e7b4e97447d31
badgeminer <64038298+badgeminer@users.noreply.github.com>
2022-07-27 09:30:22 -0600

    Update domains.py

--- dns/domains.py
+++ dns/domains.py
@@ -23,29 +23,6 @@ class Domain(DomainOptional):
 
 domains: Dict[str, Domain] = {
     # Please make sure to keep this sorted!
-
-    "basalt": { "cname": "pyroxenium.github.io" },
-    "brag": { "cname": "bragosmagos.github.io" },
-    "cash": { "cname": "mcjack123.github.io" },
-    "ccsimplegui": { "cname": "masongulu.github.io" },
-    "consult" : {"cname": "1Turtle.github.io"},
-    "craftos-pc": { "cname": "admiring-shannon-be238c.netlify.app" },
-    "freax": { "cname": "freax.netlify.app" },
-    "gemstone": { "cname": "gemstone.znepb.me" },
-    "guih": { "cname": "9551-dev.github.io" },
-    "impulsive-cc": { "cname": "emeraldimpulse7.github.io" },
-    "infernity": { "cname": "infernostars.github.io" },
-    "metis": { "cname": "squiddev-cc.github.io" },
-    "music": { "cname": "autoclave.squiddev.cc" },
-    "pecdocs": { "cname": "apethesis.github.io" },
-    "phileos" : {"cname": "ryan-te.github.io"},
-    "phoenix": { "cname": "phoenix-computercraft.netlify.app" },
-    "plethora": { "cname": "squiddev-cc.github.io" },
-    "potatos": { "cname": "osmarks.net" },
-    "recrafted": { "cname": "ocawesome101.github.io" },
-    "skydocs": { "cname": "skythecodemaster.github.io" },
-    "skygui": { "cname": "skythecodemaster.github.io" },
-    "thox": { "cname": "thox.touhey.pro" },
     "wolf-os": { "cname": "cc-wolf-os.github.io" },
     "www": { "cname": "madefor.cc" },
 }
